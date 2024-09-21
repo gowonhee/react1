7fff01ac67bc48b1dc4583b02a0456e131b5be59
Wonhee <ko0413hee@gmail.com>
2024-09-21 23:00:37 +0900

    CSS Update

--- project12_port/frontend/src/pages/Introduce.jsx
+++ project12_port/frontend/src/pages/Introduce.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Introduce = () => {
   return (
     <div>
       <form className='aboutMeForm'>
-        <div>About Me</div>
+        <div className='aboutMe-Text'>About Me</div>
         <p className='aboutLine'>-----</p>
         <div className='aboutContent'>
           <div>
             <img src="/wonnie.jpg" className='profileImg'/>
           </div>
           <div className='aboutText'>
-            <h4>고 원 희 <p>Web Developer</p></h4>
-            <p>사용자 관점에서 개발하는 웹 개발자</p>
-
-            <h5>Contact.</h5>
-            <p>Email. ko0413hee@gmail.com</p>
+            <p className='Gowonhee'>고 원 희</p>
+            <p>Web Developer</p> 
+            <h5>사용자 관점에서 개발하는 웹 개발자</h5>
+            <br />
+            <h6>Contact.</h6>
+            <FontAwesomeIcon icon={faEnvelope} /> ko0413hee@gmail.com <br />
+            <FontAwesomeIcon icon={faPhone} /> 010.8786.9214
 
           </div>
         </div>
