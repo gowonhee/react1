5a21d79b539987dc0612f2e8a2ac6bb9511447b5
Wonhee <ko0413hee@gmail.com>
2024-09-22 23:51:19 +0900

    CSS 수정

--- project12_port/frontend/src/pages/Introduce.jsx
+++ project12_port/frontend/src/pages/Introduce.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Introduce = () => {
   return (
@@ -14,20 +14,28 @@ const Introduce = () => {
             <img src="/wonnie.jpg" className='profileImg'/>
           </div>
           <div className='aboutText'>
-            <p className='Gowonhee'>고 원 희</p>
-            <p>Web Developer</p> 
+            <p className='Gowonhee'>고 원 희
+            <span>Web Developer</span></p>
             <h5>사용자 관점에서 개발하는 웹 개발자</h5>
-            <br />
+            <br /><br/>
             <h6>Contact.</h6>
-            <FontAwesomeIcon icon={faEnvelope} /> ko0413hee@gmail.com <br />
-            <FontAwesomeIcon icon={faPhone} /> 010.8786.9214
-
+            <div className='contact-icons'>
+              <p><FontAwesomeIcon icon={faEnvelope} /> ko0413hee@gmail.com </p>
+              <p><FontAwesomeIcon icon={faPhone} /> 010.8786.9214</p>
+              <p><FontAwesomeIcon icon={faLocationDot} /> REPUBLIC OF KOREA, GWANGJU</p>
+            </div>
           </div>
         </div>
       </form>
 
       <form className='archiveForm'>
-        <div>Archive</div>
+        <div className='aboutMe-Text'>Archive</div>
+        <p className='aboutLine'>-----</p>
+        <div className='link-div'>
+          <div className='otherSite'><FontAwesomeIcon icon={faGithub} /></div>
+          <div className='otherSite'>NaverBlog</div>
+          <div className='otherSite'>Notion</div>
+        </div>
       </form>
     </div>
   )
