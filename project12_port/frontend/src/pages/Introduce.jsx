50813176a8399c25642b2963bc4e6249c5d84c2b
wonnie <ko0413hee@gmail.com>
2024-09-23 12:11:59 +0900

    badge 추가

--- project12_port/frontend/src/pages/Introduce.jsx
+++ project12_port/frontend/src/pages/Introduce.jsx
@@ -17,6 +17,17 @@ const Introduce = () => {
             <p className='Gowonhee'>고 원 희
             <span>Web Developer</span></p>
             <h5>사용자 관점에서 개발하는 웹 개발자</h5>
+            <br/>
+            <div className='badge'>
+              <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"/>
+              <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"  />
+              <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"/>
+              <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/><br />
+              <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white"/>
+              <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"/>
+              <img src="https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white"/>
+              <img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"/>
+            </div>
             <br /><br/>
             <h6>Contact.</h6>
             <div className='contact-icons'>
